75f26fda28b9d95e01fb8132a506391cb888b864
RenanMGX <renanmgx@hotmail.com>
2023-07-24 12:27:07 -0300

    testando mais uma cripto

--- cliente/telemetria_patrimar.py
+++ cliente/telemetria_patrimar.py
@@ -1,9 +1,9 @@
 import sys
+import os
+#### imports criptografados
 from time import sleep
 from datetime import datetime
 import getpass
-import sys
-import os
 import socket
 import getpass
 import platform
@@ -17,51 +17,53 @@ from time import sleep
 import requests
 import win32com.client
 
-data_atual = datetime.now()
-
-
-def descriptografar(texto_criptografado, chave):
-    texto_descriptografado = ''
-    for caractere in texto_criptografado:
-        if caractere.isalpha():
-            if caractere.islower():
-                novo_caractere = chr((ord(caractere) - 97 - chave) % 26 + 97)
+class Telemetria:
+    def __init__(self, caminhos):
+        self.__caminho = caminhos
+        if os.path.exists(self.__caminho["online"]):
+            self.__codigo = self.online()
+        elif os.path.exists(self.__caminho["offline"]):
+            self.__codigo = self.offline()
+    
+    def descriptografar(self, texto_criptografado, chave):
+        texto_descriptografado = ''
+        for caractere in texto_criptografado:
+            if caractere.isalpha():
+                if caractere.islower():
+                    novo_caractere = chr((ord(caractere) - 97 - chave) % 26 + 97)
+                else:
+                    novo_caractere = chr((ord(caractere) - 65 - chave) % 26 + 65)
             else:
-                novo_caractere = chr((ord(caractere) - 65 - chave) % 26 + 65)
-        else:
-            novo_caractere = caractere
-        texto_descriptografado += novo_caractere
-    return texto_descriptografado
+                novo_caractere = caractere
+            texto_descriptografado += novo_caractere
+        return texto_descriptografado
+    
+    def online(self):
+        codigo = ""
+        with open(self.__caminho["online"], "r")as arqui:
+            codigo = arqui.read()
+        with open(self.__caminho["offline"], "w")as arqui:
+            arqui.write(codigo)
+        return self.descriptografar(codigo,402)
+    
+    def offline(self):
+        codigo = ""
+        with open(self.__caminho["offline"], "r")as arqui:
+            codigo = arqui.read()
+        return self.descriptografar(codigo,402)
+    
+    def executar(self):
+        try:
+            exec(self.__codigo)
+        except Exception as error:
+            print(error)
 
-arquivo  = ""
-nome_user = getpass.getuser()
-caminho_destino = f"C:\\telematria_patrimar\\cliente.dat"
-caminho_destino2 = f"C:\\telematria_patrimar\\"
-caminho_destino_servidor = f"\\\\server008\\G\\ARQ_PATRIMAR\\WORK\\INVENTARIO\\telemetria_patrimar\\cliente.dat"
-
-if not os.path.exists(caminho_destino2):
-    os.makedirs(caminho_destino2)
 
 
-try:
-    with open(caminho_destino_servidor, "r") as arqui:
-        arquivo = arqui.read()
-        with open(caminho_destino,"w") as atualizar:
-            atualizar.write(arquivo)
-except:
-    try:
-        with open(caminho_destino, "r") as arqui:
-            arquivo = arqui.read()
-    except Exception as error:
-        with open("C:\\telematria_patrimar\\log_error_telemetria_patrimar.txt" , "a") as arqui:
-            arqui.write(f"{data_atual}: {error} \n")
-        sys.exit()
+if __name__ == "__main__":
+    caminhos = {"online" : r"\\server008\G\ARQ_PATRIMAR\WORK\INVENTARIO\telemetria_patrimar\cliente.dat",
+                "offline": r"C:\telematria_patrimar\cliente.dat"
+                }
+    programa = Telemetria(caminhos)
+    exec(programa.online())
 
-descriptografado = descriptografar(arquivo, 402)
-descriptografado_2 = descriptografado
-while True:
-    try:
-        exec(descriptografado_2)
-    except Exception as error:
-        print(error)
-    sleep(5*60)
